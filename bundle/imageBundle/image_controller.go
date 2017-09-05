e62763235c7af08bf134b181b33d1c1b91a14d71
Hilmar Wiegand <me@hwgnd.de>
2017-09-05 23:45:34 +0200

    :sparkles: Implement image resizing service

--- bundle/imageBundle/image_controller.go
+++ bundle/imageBundle/image_controller.go
@@ -8,10 +8,11 @@ import (
 	"strconv"
 	"strings"
 
-	_ "image/jpeg"
+	"image/jpeg"
 	_ "image/png"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nfnt/resize"
 	"github.com/oliamb/cutter"
 	"github.com/satori/go.uuid"
 
@@ -22,33 +23,11 @@ const thumbWidth = 800
 const thumbHeight = 200
 
 const fullWidth = 1920
-const fullHeight = 600
+const fullHeight = 720
 
 type imageController struct{}
 
 func (imageController) Upload(c *gin.Context) {
-	uuid := uuid.NewV4()
-
-	cropTop := 0
-	cropLeft := 0
-
-	var err error
-	if c.PostForm("top") != "" {
-		cropTop, err = strconv.Atoi(c.PostForm("top"))
-		if err != nil {
-			app.BadRequest(c, errors.New("Invalid top position"))
-			return
-		}
-	}
-
-	if c.PostForm("left") != "" {
-		cropLeft, err = strconv.Atoi(c.PostForm("left"))
-		if err != nil {
-			app.BadRequest(c, errors.New("Invalid left position"))
-			return
-		}
-	}
-
 	file, err := c.FormFile("file")
 	if err != nil {
 		app.BadRequest(c, err)
@@ -67,11 +46,10 @@ func (imageController) Upload(c *gin.Context) {
 		return
 	}
 
+	uuid := uuid.NewV4()
 	pathOriginal := fmt.Sprintf("%v/%v-o.%v", app.StoragePath, uuid, parts[1])
-	pathThumb := fmt.Sprintf("%v/%v-thumb.%v", app.StoragePath, uuid, parts[1])
-	pathFull := fmt.Sprintf("%v/%v-full.%v", app.StoragePath, uuid, parts[1])
-
-	fmt.Printf("o: %v, thumb: %v, full: %v", pathOriginal, pathThumb, pathFull)
+	pathThumb := fmt.Sprintf("%v/%v-thumb.jpg", app.StoragePath, uuid)
+	pathFull := fmt.Sprintf("%v/%v-full.jpg", app.StoragePath, uuid)
 
 	// save original file to disk
 	err = c.SaveUploadedFile(file, pathOriginal)
@@ -92,6 +70,75 @@ func (imageController) Upload(c *gin.Context) {
 		app.ServerError(c, err)
 		return
 	}
+	fileOriginal.Close()
+
+	originalBounds := imgOriginal.Bounds()
+
+	x := getDefaultInt(c, "x", originalBounds.Max.X/2)
+	y := getDefaultInt(c, "y", originalBounds.Max.Y/2)
+
+	croppedFull, err := cutter.Crop(imgOriginal, cutter.Config{
+		Width:   16,
+		Height:  6,
+		Anchor:  image.Point{x, y},
+		Mode:    cutter.Centered,
+		Options: cutter.Ratio,
+	})
+
+	if err != nil {
+		app.ServerError(c, err)
+		return
+	}
+
+	thumbX := getDefaultInt(c, "thumbx", originalBounds.Max.X/2)
+	thumbY := getDefaultInt(c, "thumby", originalBounds.Max.Y/2)
+
+	croppedThumb, err := cutter.Crop(imgOriginal, cutter.Config{
+		Width:   16,
+		Height:  4,
+		Anchor:  image.Point{thumbX, thumbY},
+		Mode:    cutter.Centered,
+		Options: cutter.Ratio,
+	})
+
+	if err != nil {
+		app.ServerError(c, err)
+		return
+	}
+
+	imgFull := resize.Resize(uint(fullWidth), uint(fullHeight), croppedFull, resize.Lanczos3)
+	imgThumb := resize.Resize(uint(thumbWidth), uint(thumbHeight), croppedThumb, resize.Bilinear)
+
+	fullOut, err := os.Create(pathFull)
+	if err != nil {
+		app.ServerError(c, err)
+		return
+	}
+	defer fullOut.Close()
+
+	thumbOut, err := os.Create(pathThumb)
+	if err != nil {
+		app.ServerError(c, err)
+		return
+	}
+	defer thumbOut.Close()
+
+	jpeg.Encode(fullOut, imgFull, nil)
+	jpeg.Encode(thumbOut, imgThumb, nil)
 
 	app.Ok(c, gin.H{"name": uuid})
+}
+
+func getDefaultInt(c *gin.Context, name string, def int) int {
+	val := c.DefaultPostForm(name, strconv.Itoa(def))
+	i, err := strconv.Atoi(val)
+	if err != nil {
+		return 0
+	}
+
+	if i < 0 {
+		return 0
+	}
+
+	return i
 }
